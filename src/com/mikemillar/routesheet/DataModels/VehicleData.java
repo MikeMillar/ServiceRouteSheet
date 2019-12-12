0a74a32747d3a629c663b07de0fdcb677afec370
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2019-12-12 15:17:02 -0700

    create customer data model
    add basic customer data model

--- src/com/mikemillar/routesheet/DataModels/VehicleData.java
+++ src/com/mikemillar/routesheet/DataModels/VehicleData.java
@@ -1,4 +1,52 @@
 package com.mikemillar.routesheet.DataModels;
 
+import java.util.HashMap;
+
 public class VehicleData {
+    
+    private static VehicleData instance = new VehicleData();
+    
+    private static final String VEHICLE_DATA_FILE = "vehicles.xml";
+    
+    private HashMap<String, Vehicle> vehicleHashMap;
+    
+    public VehicleData() {
+        vehicleHashMap = new HashMap<>();
+    }
+    
+    public static VehicleData getInstance() {
+        return instance;
+    }
+    
+    public void addVehicle(Vehicle vehicle) {
+        try {
+            vehicleHashMap.put(vehicle.getShortId(), vehicle);
+        } catch (NullPointerException e) {
+            System.out.println("Unable to add vehicle");
+            e.printStackTrace();
+        }
+    }
+    
+    public void removeVehicle(Vehicle vehicle) {
+        try {
+            vehicleHashMap.remove(vehicle.getShortId());
+        } catch (NullPointerException e) {
+            System.out.println("Unable to find / remove vehicle");
+            e.printStackTrace();
+        }
+    }
+    
+    public Vehicle getVehicle(String vehicleID) {
+        try {
+            return vehicleHashMap.get(vehicleID);
+        } catch (NullPointerException e) {
+            System.out.println("Unable to find vehicle");
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public HashMap<String, Vehicle> getVehicleHashMap() {
+        return vehicleHashMap;
+    }
 }
